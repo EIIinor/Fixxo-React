92216b012473486cf4e0d608b6d2fd78701ea27b
Ellal <ellaljus@hotmail.comm>
2022-10-18 15:51:40 +0200

    card

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import CompareView from './views/CompareView';
 import WishListView from './views/WishListView';
 import ShoppingCartView from './views/ShoppingCartView';
 import NotFoundView from './views/NotFoundView';
-import FooterSection from './sections/FooterSection';
 
 
 function App() {
