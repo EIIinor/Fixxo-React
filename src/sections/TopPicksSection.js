ddcab702b8256aed29127336e854ed721666e783
Ellal <ellaljus@hotmail.comm>
2022-10-21 13:34:53 +0200

    toppicks

--- src/sections/TopPicksSection.js
+++ src/sections/TopPicksSection.js
@@ -1,29 +1,36 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import PamelaImg from '../assets/images/pamela-img.svg';
-import ConsciousImg from '../assets/images/conscious-img.svg';
-
+import PamelaImg from '../assets/images/pamela.png';
+import ConsciousImg from '../assets/images/conscious-img.png';
 const TopPicksSection = () => {
   return (
     <section className='top-picks'>
         <div className='container'>
             <div className='shop-now'>
-                <img src={PamelaImg} alt='' />
-                <h1>Pamela Rief's <span>Top Picks</span></h1>
-                <NavLink to="#" className="btn-theme">
-                    <span className='corner-left'></span>
-                    <span className='corner-right'></span>
-                    SHOP NOW
-                </NavLink>
+                <div className='image'>
+                    <img src={PamelaImg} alt='' />
+                </div>
+                <div className='info'>
+                    <h1>Pamela Rief's <span>Top Picks</span></h1>
+                    <NavLink to="#" className="btn-theme-dark">
+                        <span className='corner-left'></span>
+                        <span className='corner-right'></span>
+                        SHOP NOW
+                    </NavLink>
+                </div>
             </div>
             <div className='flash-sale'>
-                <img src={ConsciousImg} alt='' />
-                <h1>Lets Be <span>Conscious</span></h1>
-                <NavLink to="#" className="btn-theme">
-                    <span className='corner-left'></span>
-                    <span className='corner-right'></span>
-                    FLASH SALE
-                </NavLink>
+                <div className='image'>
+                    <img src={ConsciousImg} alt='' />
+                </div>
+                <div className='info'>
+                    <h1>Lets Be <span>Conscious</span></h1>
+                    <NavLink to="#" className="btn-theme-white">
+                        <span className='corner-left'></span>
+                        <span className='corner-right'></span>
+                        FLASH SALE
+                    </NavLink>
+                </div>
             </div>
         </div>
     </section>
