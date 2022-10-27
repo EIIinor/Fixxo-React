4d5b129d7ccfb087f31b508ca1cc4820d789d4d4
EIIinor <ellinor.ljus@utb.ecutbildning.se>
2022-10-27 11:00:44 +0200

    tweaks

--- src/sections/ContactFormSection.js
+++ src/sections/ContactFormSection.js
@@ -8,26 +8,37 @@ const ContactFormSection = () => {
     const validate = (values) => {
         const errors = {}
         const regex_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        const regex_name = /^[A-Za-z]+$/
+ 
 
         if(!values.name)
-            errors.name = "You must enter a name"
+            errors.name = "Tell us your name please"
+        else if(values.name.length < 2)
+        errors.name = "Too short darling, minimum two characters."
+        else if(values.name.length > 15)
+        errors.name = "Damn that is a long name, maximum 15 characters"
+        else if(!regex_name.test(values.name))
+        errors.name = "Ony letters in your name, thank u"
+        
 
         if(!values.email)
-        errors.email = "You must enter an e-mail address"
+        errors.email = "We need your e-mail address, don't be shy"
         else if(!regex_email.test(values.email))
         errors.email = "You must enter a valid e-mail address (eg. name@domain.com)"
 
         if(!values.comment)
-        errors.comment = "You must enter a comment"
+        errors.comment = "Leave a comment would you"
         else if(values.comment.length < 5)
-        errors.comment = "You comment must be minimum five characters"
+        errors.comment = "Give us at least five characters please"
+        else if(values.comment.length > 300)
+        errors.comment = "That is too long, ain't nobody got time for that, maximum 300 characters."
 
         if(Object.keys(errors).length === 0)
-            setSubmitted(true)
+           setSubmitted(true)
         else    
-            setSubmitted(false)
+         setSubmitted(false)
 
-        return errors;
+        return errors; 
     }
 
     const handleChange = (e) => {
