e0d0b7dc003c8e38125b76da8ed89601616cd1f4
EIIinor <ellinor.ljus@utb.ecutbildning.se>
2022-11-01 14:31:02 +0100

    winter fashion section

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -7,6 +7,9 @@ import TopPicksSection from '../sections/TopPicksSection'
 import OfferAndCardsSectionOne from '../sections/OfferAndCardsSectionOne'
 import OfferAndCardsSectionTwo from '../sections/OfferAndCardsSectionTwo'
 import CustomerInfoSection from '../sections/CustomerInfoSection'
+import WinterClearenceSection from '../sections/WinterClearenceSection'
+import OurSpecialitySection from '../sections/OurSpecialitySection'
+import WinterFashionSection from '../sections/WinterFashionSection'
 import { ProductContext } from '../contexts/contexts'
 
 const HomeView = () => {
@@ -20,10 +23,13 @@ const HomeView = () => {
         <MainMenuSection />
         <ShowcaseSection />
       </header>
+      <WinterClearenceSection />
       <ProductGridSection title="Featured Products" items={productContext.featuredProducts} />
       <TopPicksSection />
+      <OurSpecialitySection />
       <OfferAndCardsSectionOne items={productContext.four} />
       <OfferAndCardsSectionTwo items={productContext.four} />
+      <WinterFashionSection />
       <CustomerInfoSection />
       <FooterSection />
     </>
