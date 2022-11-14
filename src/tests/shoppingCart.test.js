8cc26bf7f0b7ca5218488625d0c626ad13cb5ec0
EIIinor <ellinor.ljus@utb.ecutbildning.se>
2022-11-14 08:28:15 +0100

    done

---
+++ src/tests/shoppingCart.test.js
@@ -0,0 +1,17 @@
+// import { render, fireEvent } from '@testing-library/react'
+// import ShoppingCart from '../components/ShoppingCart'
+
+
+// describe(ShoppingCart, () => {
+
+//    it('shoppingcart item should increment by 1 if the increment button is pressed', () => {
+//       const {getByTestId, getByRole} = render (<ShoppingCart init={1} />)
+//       const button = getByRole('button', {name:'-'})
+//
+//       fireEvent.click(button)
+//        const value = Number(getByTestId('').textContent)
+
+//       expect(value).toEqual(2)
+//    })
+
+// })
